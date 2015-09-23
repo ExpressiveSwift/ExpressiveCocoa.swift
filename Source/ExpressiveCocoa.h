3c5eb9dd8a56cc0a2f9891ced6b3b0000e51eab7
Andrey Tarantsov <andrey@tarantsov.com>
2015-09-23 15:38:57 +0600

    Skeleton

---
+++ Source/ExpressiveCocoa.h
@@ -0,0 +1,7 @@
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ExpressiveCocoa.
+FOUNDATION_EXPORT double ExpressiveCocoaVersionNumber;
+
+//! Project version string for ExpressiveCocoa.
+FOUNDATION_EXPORT const unsigned char ExpressiveCocoaVersionString[];
