e9b4afcbc454fbdf89df4b050f346125aceaaa2c
Andrey Tarantsov <andrey@tarantsov.com>
2015-10-01 04:09:33 +0600

    🚀 Add ATGlobals and ATChildTask

--- Source/ExpressiveCocoa.h
+++ Source/ExpressiveCocoa.h
@@ -5,3 +5,6 @@ FOUNDATION_EXPORT double ExpressiveCocoaVersionNumber;
 
 //! Project version string for ExpressiveCocoa.
 FOUNDATION_EXPORT const unsigned char ExpressiveCocoaVersionString[];
+
+#import <ExpressiveCocoa/ATGlobals.h>
+#import <ExpressiveCocoa/ATChildTask.h>
